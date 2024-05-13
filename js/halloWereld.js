5899b60f1b7c941259d2794ca0f91a409a1c948c
Nielsvanveen017 <niels.oa.van.veen@gmail.com>
2024-05-13 13:10:41 +0200

    index link veranderd en javascript

--- js/halloWereld.js
+++ js/halloWereld.js
@@ -2,11 +2,17 @@ console.log("Test mijn bestand wereld");
 
 // Deze data gebruik ik om mijn functie te testen, de data moet later veranderd worden aan de hand van de user input
 let goedeAntwoord = "Bellingham" 
-let userInput = "Geen Bellingham"
+let antwoord_gebruiker = "Niet Bellingham"
 
 // Aantal pogingen opslaan en ophalen
 var pogingen = 3;
 var getalPogingen = document.querySelector("#id_pogingen");
+
+// Ophalen eigenschappen speler
+var vlag = document.querySelector("id#vlag");
+var positie = document.querySelector("#positie");
+var competitie = document.querySelector("#competitie");
+
 
 // Een functie die zonder op de knop te drukken aangeeft hoeveel pogingen de gebruiker nog over heeft
 function default_number(){
@@ -25,7 +31,16 @@ default_number()
 
 // Ophalen tekst foutmelding zodat deze gewijzigd kan worden
 var text_foutmelding = document.querySelector("#id_foutmelding");
-var antwoord_gebruiker = document.querySelector("#userInput")
+
+// Ophalen data
+var vlagSpeler = document.querySelector("#vlag");
+var competitieSpeler = document.querySelector("#competitie");
+
+// Functie voor het genereren van een nieuwe speler
+function generate_player(){
+    positie.textContent = "Aanvaller";  
+}
+
 
 // Functie om user input te checken, nog niet helemaal volledig
 function check_input(){
@@ -33,7 +48,8 @@ function check_input(){
         text_foutmelding.textContent = "Helaas dat is fout, je hebt nog " + pogingen +  " pogingen om het te proberen."
     }
     else{
-        text_foutmelding.textContent = ""
+        text_foutmelding.textContent = "Gefeliciteerd, jouw antwoord was goed"
+        generate_player()
     }
 }
 
@@ -41,3 +57,5 @@ function check_input(){
 var checkButton = document.querySelector("button");
 checkButton.addEventListener("click", veranderPogingen);
 checkButton.addEventListener("click", check_input);
+
+
