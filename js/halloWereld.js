2503fff389aabd7712ec41da597f3918d5a9b8f4
Nielsvanveen017 <niels.oa.van.veen@gmail.com>
2024-05-13 16:01:00 +0200

    update structuur van code

--- js/halloWereld.js
+++ js/halloWereld.js
@@ -1,17 +1,15 @@
-console.log("Test mijn bestand wereld");
+// Ophalen foutmelding en knop
+let text_foutmelding = document.querySelector("#id_foutmelding");
+let checkButton = document.querySelector("button");
 
-// Deze data gebruik ik om mijn functie te testen, de data moet later veranderd worden aan de hand van de user input
+// Ophalen data over het antwoord & pogingen
 let goedeAntwoord = "Bellingham" 
 let antwoord_gebruiker = "Niet Bellingham"
+let pogingen = 3;
+let getalPogingen = document.querySelector("#id_pogingen");
 
-// Aantal pogingen opslaan en ophalen
-var pogingen = 3;
-var getalPogingen = document.querySelector("#id_pogingen");
-
-// Ophalen eigenschappen speler
-var vlag = document.querySelector("id#vlag");
-var positie = document.querySelector("#positie");
-var competitie = document.querySelector("#competitie");
+// Ophalen data speler
+let afbeeldingSpeler = document.querySelector("#playerDetails");
 
 
 // Een functie die zonder op de knop te drukken aangeeft hoeveel pogingen de gebruiker nog over heeft
@@ -21,41 +19,33 @@ function default_number(){
 
 // Met behulp van deze functie verander je het aantal pogingen in de HTMl
 function veranderPogingen(){
-        pogingen = pogingen - 1
-        console.log("Pogingen: " + pogingen)
-        getalPogingen.textContent = pogingen
+    pogingen--;
+    console.log("Pogingen: " + pogingen)
+    getalPogingen.textContent = pogingen
 }
 
-// Functie aanroepen zodat hij getoond wordt
-default_number()
-
-// Ophalen tekst foutmelding zodat deze gewijzigd kan worden
-var text_foutmelding = document.querySelector("#id_foutmelding");
-
-// Ophalen data
-var vlagSpeler = document.querySelector("#vlag");
-var competitieSpeler = document.querySelector("#competitie");
-
-// Functie voor het genereren van een nieuwe speler
+// Functie voor het genereren van een nieuwe speler (nog niet af)
 function generate_player(){
-    positie.textContent = "Aanvaller";  
 }
-
 
 // Functie om user input te checken, nog niet helemaal volledig
 function check_input(){
-    if (antwoord_gebruiker !== goedeAntwoord){
+    if (pogingen <= 0){
+        text_foutmelding.textContent = "Helaas dat is fout, je hebt geen pogingen meer om het te proberen."
+    }
+    else if (antwoord_gebruiker !== goedeAntwoord){
         text_foutmelding.textContent = "Helaas dat is fout, je hebt nog " + pogingen +  " pogingen om het te proberen."
     }
+
     else{
         text_foutmelding.textContent = "Gefeliciteerd, jouw antwoord was goed"
         generate_player()
     }
 }
+// Functie aanroepen zodat hij getoond wordt
+default_number()
 
 // Verander het aantal pogingen pas wanneer de gebruiker op de knop drukt
-var checkButton = document.querySelector("button");
 checkButton.addEventListener("click", veranderPogingen);
 checkButton.addEventListener("click", check_input);
-
 
