1a8dd784d65c0ce091de1ad22aa2fa1002356059
Nielsvanveen017 <niels.oa.van.veen@gmail.com>
2024-05-16 14:14:19 +0200

    Update na hulp van Robin

--- js/halloWereld.js
+++ js/halloWereld.js
@@ -1,51 +1,104 @@
 // Ophalen foutmelding en knop
-let text_foutmelding = document.querySelector("#id_foutmelding");
+let text_foutmelding = document.getElementById("id_foutmelding");
 let checkButton = document.querySelector("button");
-
-// Ophalen data over het antwoord & pogingen
-let goedeAntwoord = "Bellingham" 
-let antwoord_gebruiker = "Niet Bellingham"
-let pogingen = 3;
-let getalPogingen = document.querySelector("#id_pogingen");
+let userInput = document.getElementById("userInput");
 
 // Ophalen data speler
-let afbeeldingSpeler = document.querySelector("#playerDetails");
+let afbeeldingSpeler = document.getElementById("playerDetails");
 
+let getalPogingen = document.getElementById("id_pogingen");
+
+// Ophalen data over het antwoord & pogingen
+let questions = [];
+
+// standaard leeg antwoord voor de gebruiker 
+let antwoord_gebruiker = ""
+let pogingen = 3;
+
+let actieveVraag = {}
+
+// Haal de data van de vragen op uit het json bestand
+fetch('../questions.json')
+    .then(response => response.json())
+    .then(jsonData => {
+        questions = jsonData;
+        // console.log('Questions', questions);
+        generate_player()
+    })
+    .catch(error => console.error('Error fetching JSON data:', error));
 
 // Een functie die zonder op de knop te drukken aangeeft hoeveel pogingen de gebruiker nog over heeft
-function default_number(){
+function default_number() {
+    pogingen = 3;
     getalPogingen.textContent = pogingen;
 }
 
 // Met behulp van deze functie verander je het aantal pogingen in de HTMl
-function veranderPogingen(){
-    pogingen--;
-    console.log("Pogingen: " + pogingen)
-    getalPogingen.textContent = pogingen
+function veranderPogingen() {
+    if (pogingen > 0) {
+        pogingen--;
+        getalPogingen.textContent = pogingen
+    }
+}
+
+// pick a random record from the array in javascript
+function random_vraag(array) {
+    const randomIndex = Math.floor(
+        Math.random() * array.length
+    );
+    return array[randomIndex];
 }
 
 // Functie voor het genereren van een nieuwe speler (nog niet af)
-function generate_player(){
+function generate_player() {
+    if (questions.length > 0) {
+        actieveVraag = random_vraag(questions);
+
+        afbeeldingSpeler.src = actieveVraag.image
+
+        console.log(afbeeldingSpeler, actieveVraag)
+    }
 }
 
-// Functie om user input te checken, nog niet helemaal volledig
-function check_input(){
-    if (pogingen <= 0){
-        text_foutmelding.textContent = "Helaas dat is fout, je hebt geen pogingen meer om het te proberen."
-    }
-    else if (antwoord_gebruiker !== goedeAntwoord){
-        text_foutmelding.textContent = "Helaas dat is fout, je hebt nog " + pogingen +  " pogingen om het te proberen."
-    }
+// krijg de value van de input wat de gebruiker invoert en zet deze als antwoord voor de gebruiker
+function changeUserInput() {
+    // Zet antwoord van gebruiker uit de input in de aangemaakte lege string
+    antwoord_gebruiker = userInput.value;
+}
 
-    else{
-        text_foutmelding.textContent = "Gefeliciteerd, jouw antwoord was goed"
-        generate_player()
+
+// Functie om user input te checken, nog niet helemaal volledig
+function check_input() {
+    // Check of de actieve vraag is gezet en niet leeg is -> check data succesvol opgehaald
+    if (actieveVraag) {
+        // Check of de gebruiker nog pogingen over heeft
+        if (pogingen <= 0) {
+            text_foutmelding.textContent = "Helaas dat is fout, je hebt geen pogingen meer om het te proberen."
+        } 
+        // Check antwoord gebruiker met correct antwoord
+        else if (antwoord_gebruiker !== actieveVraag.correct_answer) {
+            veranderPogingen()
+            text_foutmelding.textContent = "Helaas dat is fout, je hebt nog " + pogingen + " pogingen om het te proberen."
+        } 
+        // geef correct antwoord als dit geraden is
+        else {
+            text_foutmelding.textContent = "Gefeliciteerd, jouw antwoord was goed";
+            default_number()
+            generate_player()
+            userInput.value = ""
+        }
     }
 }
 // Functie aanroepen zodat hij getoond wordt
 default_number()
 
 // Verander het aantal pogingen pas wanneer de gebruiker op de knop drukt
-checkButton.addEventListener("click", veranderPogingen);
-checkButton.addEventListener("click", check_input);
+// checkButton.addEventListener("click", veranderPogingen);
+// checkButton.addEventListener("click", check_input);
+
+checkButton.addEventListener("click", function () {
+    // veranderPogingen() 
+    check_input()
+})
+
 
