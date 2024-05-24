e7bccd9235b76e535cde79c52084bb198a4881db
Nielsvanveen017 <niels.oa.van.veen@gmail.com>
2024-05-24 11:36:41 +0200

    Changes na Lisette haar hulp

--- js/halloWereld.js
+++ js/halloWereld.js
@@ -1,66 +1,76 @@
 // Ophalen foutmelding en knop
-let text_foutmelding = document.getElementById("id_foutmelding");
+const textFoutmelding = document.getElementById("id_foutmelding");
 let checkButton = document.querySelector("button");
 let userInput = document.getElementById("userInput");
 
 // Ophalen data speler
 let afbeeldingSpeler = document.getElementById("playerDetails");
-
 let getalPogingen = document.getElementById("id_pogingen");
 
-// De questions zijn leeg zodat ze later gevuld kunnen worden met data uit het json bestand
-let questions = [];
+let questions = [
+    {
+        "image": "../Images/Bellingham.svg",
+        "correct_answer": "Jude Bellingham"
+    },
+    {
+        "image": "../Images/simons.svg",
+        "correct_answer": "Xavi Simons"
+    },
+    {
+        "image": "../Images/mbape.svg",
+        "correct_answer": "Mbape"
+    },
+    {
+        "image": "../Images/haaland.svg",
+        "correct_answer": "Haaland"
+    },
+    {
+        "image": "../Images/salah.svg",
+        "correct_answer": "Salah"
+    },
+    {
+        "image": "../Images/debruyne.svg",
+        "correct_answer": "De Bruyne"
+    },
+    {
+        "image": "../Images/wirtz.svg",
+        "correct_answer": "Wirtz"
+    }
+];
 
 // standaard leeg antwoord voor de gebruiker zodat deze later gevuld kan worden met de input van de gebruiker
-let antwoord_gebruiker = ""
+let antwoordGebruiker = ""
 let pogingen = 3;
-let actieveVraag = {}
-
-
-
-// Haal de data van de vragen op uit het json bestand (ChatGPT en collega)
-fetch('../questions.json')
-    .then(response => response.json())
-    .then(jsonData => {
-        questions = jsonData;
-        // console.log('Questions', questions);
-        generate_player()
-    })
-    .catch(error => console.error('Error fetching JSON data:', error));
+let randomVraag;
+let actieveVraag = {};
 
 // Een functie die zonder op de knop te drukken aangeeft hoeveel pogingen de gebruiker nog over heeft
-function default_number() {
+function defaultNumber() {
     pogingen = 3;
     getalPogingen.textContent = pogingen;
 }
 
 // Met behulp van deze functie verander je het aantal pogingen in de HTMl
 function veranderPogingen() {
+    console.log("verander pogingen functie");
+    console.log("pogingen: " + pogingen);
     if (pogingen > 0) {
         pogingen--;
-        getalPogingen.textContent = pogingen
+        getalPogingen.textContent = pogingen;
     }
 }
 
-// pick a random record from the array in javascript (ChatGPT en collega) --> zorgt ervoor dat er een random vraag uit questions (gevuld met json data) wordt gekozen
-function random_vraag(array) {
-    const randomIndex = Math.floor(
-        Math.random() * array.length
-    );
-    return array[randomIndex];
+function changeUserInput() {
+    // Zet antwoord van gebruiker uit de input in de aangemaakte lege string
+    antwoordGebruiker = userInput.value;
 }
 
 // Functie voor het genereren van een nieuwe speler
-function generate_player() {
-        actieveVraag = random_vraag(questions);
-        afbeeldingSpeler.src = actieveVraag.image
-        console.log(afbeeldingSpeler, actieveVraag)
-    }
-
-// krijg de value van de input wat de gebruiker invoert en zet deze als antwoord voor de gebruiker
-function changeUserInput() {
-    // Zet antwoord van gebruiker uit de input in de aangemaakte lege string
-    antwoord_gebruiker = userInput.value;
+function generatePlayer() {
+    defaultNumber();
+    randomVraag = Math.floor(Math.random()*7);
+    actieveVraag = questions[randomVraag];
+    afbeeldingSpeler.src = actieveVraag.image;
 }
 
 function playMusic(audioFilePath) {
@@ -68,51 +78,41 @@ function playMusic(audioFilePath) {
     audio.play();
 }
 
-// Functie om user input te checken
-function check_input() {
-    // Check of de actieve vraag is gezet en niet leeg is -> check data succesvol opgehaald
-    if (actieveVraag) {
-        // Check of de gebruiker nog pogingen over heeft
-        if (pogingen <= 1) {
-            text_foutmelding.textContent = "Helaas dat is fout, je hebt geen pogingen meer om het te proberen."
-            text_foutmelding.style.color = '#EE2424'
-            default_number()
-            generate_player()
-            playMusic("../sound-effects/geenPogingen.mp3") 
-            // Resetten van user input (zodat input leeg wordt)
-            userInput.value = "";
-            timeoutDuration = 3500;
-        } 
-        // Check antwoord gebruiker met correct antwoord
-        else if (antwoord_gebruiker !== actieveVraag.correct_answer) {
-            veranderPogingen()
-            text_foutmelding.textContent = "Helaas dat is fout, je hebt nog " + pogingen + " pogingen om het te proberen."
-            text_foutmelding.style.color = '#EE2424'
-            playMusic("../sound-effects/wrong.mp3")
-            timeoutDuration = 3000; 
-        } 
-        // geef correct antwoord als dit geraden is
-        else {
-            text_foutmelding.textContent = "Gefeliciteerd, jouw antwoord was goed";
-            text_foutmelding.style.color = '#32D74B'
-            playMusic("../sound-effects/correct.mp3") 
-            default_number()
-            generate_player()
-            userInput.value = "";
-            timeoutDuration = 2000;
-        }
+function feedback(text, kleur, timeoutDuration){
+    textFoutmelding.textContent = text;
+    textFoutmelding.style.color = kleur;
+    userInput.value = "";
+    
+    // Zorg ervoor dat de melding verdwijnd. Dit is verschillend per type foutmelding, waardoor ik timeoutDuration heb gebruikt 
+    setTimeout(() => {
+        textFoutmelding.textContent = "";
+    }, timeoutDuration);
+}
 
-        // Zorg ervoor dat de melding verdwijnd. Dit is verschillend per type foutmelding, waardoor ik timeoutDuration heb gebruikt
-        setTimeout(() => {
-            text_foutmelding.textContent = "";
-        }, timeoutDuration);
+// Functie om user input te checken
+function checkInput() {
+    // Check of de gebruiker nog pogingen over heeft
+    if (pogingen <= 1) {
+        feedback("Helaas dat is fout, je hebt geen pogingen meer om het te proberen.", "#EE2424", 3500);
+        playMusic("../sound-effects/geenPogingen.mp3");
+        generatePlayer();
+    } 
+    // Check antwoord gebruiker met correct antwoord
+    else if (antwoordGebruiker !== actieveVraag.correct_answer) {
+        veranderPogingen();
+        feedback("Helaas dat is fout, je hebt nog " + pogingen + " pogingen om het te proberen.", "#EE2424", 3000 );
+        playMusic("../sound-effects/wrong.mp3");
+    } 
+    // geef correct antwoord als dit geraden is
+    else {
+        feedback("Gefeliciteerd, jouw antwoord was goed", '#32D74B', 2000);
+        playMusic("../sound-effects/correct.mp3");
+        generatePlayer();
     }
 }
 
-default_number()
-
+generatePlayer();
 checkButton.addEventListener("click", function () {
-    check_input()
+    checkInput();
 })
-
 
